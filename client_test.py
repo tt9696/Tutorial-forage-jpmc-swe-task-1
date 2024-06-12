126904a0dc7e6c1a1cddea63bde714a52b4d1dbc
tt9696 <taytayshiek@gmail.com>
2024-06-12 17:17:17 +0800

    Resolved merge conflicts

--- client_test.py
+++ client_test.py
@@ -1,5 +1,6 @@
 import unittest
 from client3 import getDataPoint
+from client3 import getRatio
 
 class ClientTest(unittest.TestCase):
   def test_getDataPoint_calculatePrice(self):
@@ -8,6 +9,8 @@ class ClientTest(unittest.TestCase):
       {'top_ask': {'price': 121.68, 'size': 4}, 'timestamp': '2019-02-11 22:06:30.572453', 'top_bid': {'price': 117.87, 'size': 81}, 'id': '0.109974697771', 'stock': 'DEF'}
     ]
     """ ------------ Add the assertion below ------------ """
+    for quote in quotes:
+        self.assertEqual(getDataPoint(quote), (quote['stock'], quote['top_bid']['price'], quote['top_bid']['price'] + quote['top_bid']['price'])/2))
 
   def test_getDataPoint_calculatePriceBidGreaterThanAsk(self):
     quotes = [
@@ -15,11 +18,20 @@ class ClientTest(unittest.TestCase):
       {'top_ask': {'price': 121.68, 'size': 4}, 'timestamp': '2019-02-11 22:06:30.572453', 'top_bid': {'price': 117.87, 'size': 81}, 'id': '0.109974697771', 'stock': 'DEF'}
     ]
     """ ------------ Add the assertion below ------------ """
-
+    for quote in quotes:
+        self.assertEqual(getDataPoint(quote), (quote['stock'], quote['top_bid']['price'], quote['top_bid']['price'] + quote['top_bid']['price'])/2))
 
   """ ------------ Add more unit tests ------------ """
+  def test_getRatio_priceBZero(self):
+        self.assertIsNone(getRatio(100, 0)) #price B is 0
+        #self.assertEqual(getRatio(100, 0), None)
 
+  def test_getRatio_priceAZero(self):
+        self.assertEqual(getRatio(0, 100), 0.0)
 
+  def test_getRatio_nonZeroPrices(self):
+        self.assertEqual(getRatio(100, 50), 2.0)
+        self.assertEqual(getRatio(50, 100), 0.5)
 
 if __name__ == '__main__':
     unittest.main()
